36015439d1985a3ff22fb6a3d536c28b768cb445
shukshi <shukshiraj@gmail.com>
2022-09-05 23:36:03 +0530

    setting up project

---
+++ src/main/java/com/metric/oyometrics_team2/OyoMetricsTeam2Application.java
@@ -0,0 +1,13 @@
+package com.metric.oyometrics_team2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OyoMetricsTeam2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OyoMetricsTeam2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/metric/oyometrics_team2/OyoMetricsTeam2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metric.oyometrics_team2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OyoMetricsTeam2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
