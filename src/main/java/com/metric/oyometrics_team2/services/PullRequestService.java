554e392d6b9acb4d5605bcbfeb8e644cbe94f692
vamshivutnoori <vutnoorivamshi2@gmail.com>
2022-09-12 20:34:54 +0530

    Merged API: Merge frequency

--- src/main/java/com/metric/oyometrics_team2/services/PullRequestService.java
+++ src/main/java/com/metric/oyometrics_team2/services/PullRequestService.java
@@ -1,11 +1,15 @@
 package com.metric.oyometrics_team2.services;
 
 import com.metric.oyometrics_team2.DTO.PullRequest;
+import com.metric.oyometrics_team2.DTO.PullRequestResponse;
 import com.metric.oyometrics_team2.external.GithubApiManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
@@ -13,25 +17,49 @@ import java.util.concurrent.atomic.AtomicReference;
 @Service
 public class PullRequestService {
 
+
     @Autowired
     private GithubApiManager githubApiManager;
-    public Long getAverageTimeSpent(String repoOwner, String repoName){
 
-        List<PullRequest> pullRequests = githubApiManager.getPullRequestData(repoOwner, repoName);
+    @Autowired
+    private PullRequestResponse pullRequestResponse;
+
+    public PullRequestResponse getAverageTimeSpent(String repoOwner, String repoName){
+        List <PullRequest> pullRequests = githubApiManager.getPullRequestData(repoOwner, repoName);
         AtomicReference<Long> totalTime = new AtomicReference<>(Long.valueOf(0));
 
+        AtomicReference<LocalDateTime> lastMergedAT = new AtomicReference<>(LocalDateTime.ofEpochSecond(10000, 0, ZoneOffset.UTC));
+        AtomicReference<LocalDateTime> firstMergedAT = new AtomicReference<>(LocalDateTime.now(ZoneId.of("Asia/Kolkata")));
+
+        AtomicReference<Long> mergedAtNotNULL = new AtomicReference<>(Long.valueOf(0));
 
         pullRequests.forEach(pulls -> {
             Duration diff = Duration.between(pulls.getCreatedAt(), pulls.getClosedAt());
             totalTime.updateAndGet(v -> v + diff.getSeconds());
+            if(pulls.getMergedAt() != null) {
+                mergedAtNotNULL.updateAndGet(v -> v + 1);
+                if (pulls.getMergedAt().isAfter(lastMergedAT.get())) {
+                    lastMergedAT.set(pulls.getMergedAt());
+                }
+                if (pulls.getMergedAt().isBefore(firstMergedAT.get())) {
+                    firstMergedAT.set(pulls.getMergedAt());
+                }
+            }
         });
+        //System.out.println("WHAT hapened");
+        Long averageTimeSpent = totalTime.get()/ pullRequests.size();
+        Duration totalMergeDays = Duration.between(firstMergedAT.get(), lastMergedAT.get());
+//        System.out.println(totalMergeDays);
+//        System.out.println("Working fine");
 
+        Double totalValidDays = (double)totalMergeDays.toDays();
+        Double PrMergeFrequency = mergedAtNotNULL.get()/totalValidDays;
+        PullRequestResponse finalAns = new PullRequestResponse();
 
-        Long i = totalTime.get()/ pullRequests.size();
+        finalAns.setAverageTimeSpent(averageTimeSpent);
+        finalAns.setPRMergeFrequency(PrMergeFrequency);
 
-        System.out.println(i);
-
-        return i;
+        return finalAns;
     }
 }
 
